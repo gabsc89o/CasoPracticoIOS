d28d4ad5b393c017c098994aa63ac61d00f24b4e
gabsc89o <user138066@eu249.local>
2018-05-07 14:55:49 +0100

    Initial Commit

---
+++ CasoPracticoIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CasoPracticoIOS
+//
+//  Created by user138066 on 5/7/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CasoPracticoIOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CasoPracticoIOS
+//
+//  Created by user138066 on 5/7/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
